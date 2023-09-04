9b7072ff4f637f2bb733b83e76d024fe08ec8cba
rdbz49ers <ralph.recinto1992@gmail.com>
2023-09-04 15:48:45 -0700

    Add OOP for shapes

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-//https://nodejs.dev/en/learn/accept-input-from-the-command-line-in-nodejs/
+
+class Shape {
+    constructor(LogoAnswers) {
+        this.color = LogoAnswers.color;
+        this.text = LogoAnswers.text;
+        this.shapeColor = LogoAnswers.shapeColor;
+    }
+
+    getSVG() {
+        return '';
+    }
+}
+
+class Triangle extends Shape {
+    getSVG() {
+        return `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <polygon points="150,50 200,150 100,150" style="fill:none;stroke:${this.shapeColor};stroke-width:2" />
+      <text x="150" y="100" text-anchor="middle" style="fill:${this.color};font-size:22">${this.text}</text>
+    </svg>
+    `;
+    }
+}
+
+class Square extends Shape {
+    getSVG() {
+        return `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <rect x="100" y="50" width="100" height="100" style="fill:none;stroke-width:2;stroke:${this.shapeColor}" />
+      <text x="150" y="100" text-anchor="middle" style="fill:${this.color};font-size:22">${this.text}</text>
+    </svg>
+    `;
+    }
+}
+
+class Circle extends Shape {
+    getSVG() {
+        return `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="150" cy="100" r="50" stroke="${this.shapeColor}" stroke-width="2" fill="none" />
+      <text x="150" y="100" text-anchor="middle" style="fill:${this.color};font-size:22">${this.text}</text>
+    </svg>
+    `;
+    }
+}
 
 const LogoQuestions = [
     {
@@ -26,28 +70,26 @@ const LogoQuestions = [
     },
 ];
 
+class SVGCreator {
+    static createSVG(shapeType, LogoAnswers) {
+        switch (shapeType) {
+            case 'circle':
+                return new Circle(LogoAnswers);
+            case 'square':
+                return new Square(LogoAnswers);
+            default:
+                return new Triangle(LogoAnswers);
+        }
+    }
+}
+
 async function generateLogo() {
     await inquirer.prompt(LogoQuestions).then(LogoAnswers => {
-
-        let shapeText = "";
-        let logoShape = LogoAnswers.shape;
-        
-        if(logoShape === "circle"){
-            shapeText = `<circle cx="150" cy="100" r="50" stroke="${LogoAnswers.shapeColor}" stroke-width="2" fill="none" />`;
-        } else if (logoShape ==="square"){
-            shapeText = `<rect x="100" y="50" width="100" height="100" style="fill:none;stroke-width:2;stroke:${LogoAnswers.shapeColor}" />`;
-        } else{
-            shapeText = `<polygon points="150,50 200,150 100,150" style="fill:none;stroke:${LogoAnswers.shapeColor};stroke-width:2" />`;
-        }
-
-        let svgLogo = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        ${shapeText}
-        <text x="150" y="100" text-anchor="middle" style="fill:${LogoAnswers.color};font-size:22">${LogoAnswers.text}</text>
-        </svg>`
-        fs.writeFileSync("./logo.svg",svgLogo);
+        const shapeObject = SVGCreator.createSVG(LogoAnswers.shape, LogoAnswers);
+        fs.writeFileSync('./logo.svg', shapeObject.getSVG());
     });
 
-    console.log("Generated logo.svg");
+    console.log('Generated logo.svg');
 }
 
 generateLogo();
